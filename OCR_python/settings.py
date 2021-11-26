5e2e9bd973560988210e584f10d50f90866b85aa
Shahriar6740 <hshahriar75@gmail.com>
2021-11-27 00:51:20 +0600

    formatting settings file

--- OCR_python/settings.py
+++ OCR_python/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c=w=bf*vrhddz&cm+*t^j(sj(t@pn^9#0x!r&@5&3g7jwc5zxr'
-#SECRET_KEY = 'YOUR PROJECT SECRET KEY'
+
+SECRET_KEY = 'YOUR PROJECT SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
